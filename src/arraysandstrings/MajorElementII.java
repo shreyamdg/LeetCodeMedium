8b926f4d22e1b44c416f90e0036d703284eaff7c
shreyam15 <shreyam.duttagupta@gmail.com>
2018-09-19 22:39:43 -0500

    deleted sysout

--- src/arraysandstrings/MajorElementII.java
+++ src/arraysandstrings/MajorElementII.java
@@ -27,9 +27,4 @@ public class MajorElementII {
 		}
 		return myList;
 	}
-
-	public static void main(String[] args) {
-		int[] arr = new int[] { 1, 2, 3, 2, 3, 1, 3, 3, 1 };
-		System.out.println(majorityElement(arr));
-	}
 }
