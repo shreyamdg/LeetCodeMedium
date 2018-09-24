e5944f088d5771c0dc62a83fe84cbe74339648bd
shreyam15 <shreyam.duttagupta@gmail.com>
2018-09-23 23:00:32 -0500

    atoi in java

---
+++ src/arraysandstrings/Atoi.java
@@ -0,0 +1,42 @@
+package arraysandstrings;
+
+public class Atoi {
+	public int myAtoi(String str) {
+		// trim white spaces
+		str = str.trim();
+		if (str == null || str.length() < 1)
+			return 0;
+
+		char flag = '+';
+
+		// check negative or positive
+		int i = 0;
+		if (str.charAt(0) == '-') {
+			flag = '-';
+			i++;
+		} else if (str.charAt(0) == '+') {
+			i++;
+		}
+		// use double to store result
+		double result = 0;
+
+		// calculate value
+		while (str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+			result = result * 10 + (str.charAt(i) - '0');
+			i++;
+		}
+
+		if (flag == '-')
+			result = -result;
+
+		// handle max and min
+		if (result > Integer.MAX_VALUE)
+			return Integer.MAX_VALUE;
+
+		if (result < Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+
+		return (int) result;
+	}
+}
+
